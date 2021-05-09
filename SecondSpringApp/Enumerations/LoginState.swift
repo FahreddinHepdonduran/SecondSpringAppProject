a50ac5d610abd3110610b06c49fc77b305e516c3
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-09 23:47:20 +0300

    Login State Enum

---
+++ SecondSpringApp/Enumerations/LoginState.swift
@@ -0,0 +1,14 @@
+//
+//  LoginState.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 9.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import Foundation
+
+enum LoginState {
+  case loggedIn
+  case notLoggedIn
+}
