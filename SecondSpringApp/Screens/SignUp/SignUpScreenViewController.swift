f0ca39cf7b190ac6e374f07f9047075ee33589ea
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-09 23:40:37 +0300

    SignUp Screen

---
+++ SecondSpringApp/Screens/SignUp/SignUpScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpScreenViewController.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 9.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+final class SignUpScreenViewController: UIViewController {
+
+  @IBOutlet private weak var signUpButton: UIButton!
+  @IBOutlet private weak var loginButton: UIButton!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  @IBAction func loginButtonTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func signUpButtonTapped(_ sender: UIButton) {
+    
+  }
+  
+}
