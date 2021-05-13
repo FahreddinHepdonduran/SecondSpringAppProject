96fd166f0a0300722e9b4442ab6bcb7cc4e6cbd9
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-13 18:16:03 +0300

    complete home ui

--- SecondSpringApp/Screens/Home/HomeViewController.swift
+++ SecondSpringApp/Screens/Home/HomeViewController.swift
@@ -7,15 +7,76 @@
 //
 
 import UIKit
+import Firebase
 
 final class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+  @IBOutlet private weak var tableView: UITableView!
+  
+  private var user: User? {
+    guard let currentUser = Auth.auth().currentUser else {
+      return nil
     }
+    
+    return currentUser
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableViewDelegates()
+    configureNavigationController()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+  }
+  
+}
 
+private extension HomeViewController {
+  
+  func tableViewDelegates() {
+    tableView.delegate = self
+    tableView.dataSource = self
+  }
+  
+  func configureNavigationController() {
+    let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                             target: self,
+                                             action: #selector(rightBarButtonItemAction))
+    navigationItem.rightBarButtonItem = rightBarButtonItem
+  }
+  
+  @objc
+  func rightBarButtonItemAction() {
+    
+  }
+  
+}
+
+extension HomeViewController: UITableViewDelegate { }
+
+extension HomeViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "RoomTableViewCell") as! RoomTableViewCell
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 70
+  }
+  
 }
 
 extension HomeViewController: StoryboardInstantiable { }
