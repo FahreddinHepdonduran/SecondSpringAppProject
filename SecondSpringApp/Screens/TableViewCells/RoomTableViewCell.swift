ed073fdc86d9e7bfd252f927074c9456edca4ba6
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-13 18:15:52 +0300

    add room cell

---
+++ SecondSpringApp/Screens/TableViewCells/RoomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RoomTableViewCell.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 13.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+final class RoomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
