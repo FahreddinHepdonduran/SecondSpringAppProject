23de49de31edc2baab61765babdb7264080efe88
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-14 14:41:19 +0300

    complete chat ui

---
+++ SecondSpringApp/Screens/Chat/ChatViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChatViewController.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 14.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+final class ChatViewController: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var messageTextField: UITextField!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ SecondSpringApp/Screens/TableViewCells/ChatTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ChatTableViewCell.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 14.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+final class ChatTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var messageTextView: UITextView!
+  @IBOutlet weak var senderNicknameLabel: UILabel!
+  
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
