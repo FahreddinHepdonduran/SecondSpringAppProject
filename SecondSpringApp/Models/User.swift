880ab963d9e3fa2a1936e270a85b77c931ee0be7
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-17 23:05:25 +0300

    add user model

---
+++ SecondSpringApp/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 17.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+  let uid: String
+  let name: String
+}
