5741bed066f8ffce08976ea6e3b776932134f038
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-18 18:06:07 +0300

    seperate chat table view extensions

---
+++ SecondSpringApp/Screens/Chat/Extensions/ChatTableViewDataSourceExtension.swift
@@ -0,0 +1,23 @@
+//
+//  UITableViewDataSourceExtension.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 18.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+extension ChatViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return room.messageHistory.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ChatTableViewCell") as! ChatTableViewCell
+    cell.message = room.messageHistory[indexPath.row]
+    return cell
+  }
+  
+}
---
+++ SecondSpringApp/Screens/Chat/Extensions/ChatTableViewDelegateExtension.swift
@@ -0,0 +1,11 @@
+//
+//  TableViewDelegateExtension.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 18.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+extension ChatViewController: UITableViewDelegate {}
