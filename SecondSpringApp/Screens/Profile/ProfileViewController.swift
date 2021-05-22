27bbfa217ce8c344ee87b463f01f3510a637d304
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-22 14:27:39 +0300

    add profile screen

---
+++ SecondSpringApp/Screens/Profile/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  SecondSpringApp
+//
+//  Created by fahreddin on 22.05.2021.
+//  Copyright © 2021 fahreddin. All rights reserved.
+//
+
+import UIKit
+
+final class ProfileViewController: UITableViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
+
+// MARK: - TableViewDelegate
+extension ProfileViewController { }
+
+// MARK: - TableViewDataSoruce
+extension ProfileViewController { }
