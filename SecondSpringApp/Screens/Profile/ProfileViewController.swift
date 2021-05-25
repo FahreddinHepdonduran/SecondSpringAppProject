eecd06a00f85b0e6408010886a61184d083ac5e7
FahreddinHepdonduran <fahreddinhepdonduran@gmail.com>
2021-05-25 13:58:31 +0300

    complete profile image read write

--- SecondSpringApp/Screens/Profile/ProfileViewController.swift
+++ SecondSpringApp/Screens/Profile/ProfileViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseStorage
+import FirebaseFirestore
 
 final class ProfileViewController: UIViewController, UINavigationControllerDelegate {
   
@@ -14,21 +16,19 @@ final class ProfileViewController: UIViewController, UINavigationControllerDeleg
   @IBOutlet private weak var nameLabel: UILabel!
   @IBOutlet private weak var emailLabel: UILabel!
   
-  var user: UserInfo? {
-    didSet {
-      guard let user = user else { return }
-      nameLabel.text = user.name
-      emailLabel.text = user.email
-    }
-  }
+  var user: UserInfo!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     profilemageView.addGestureRecognizer(UITapGestureRecognizer(target: self,
                                                                 action: #selector(openPicker)))
+    nameLabel.text = user.name
+    emailLabel.text = user.email
+    downloadImage()
   }
   
   @IBAction func saveButtonDidTap(_ sender: UIButton) {
+    uploadImage()
   }
   
 }
@@ -41,6 +41,44 @@ private extension ProfileViewController {
     picker.sourceType = .photoLibrary
     picker.delegate = self
     present(picker, animated: true, completion: nil)
+  }
+  
+  func uploadImage() {
+    let image = profilemageView.image
+    let imageData = image?.jpegData(compressionQuality: 0.5)
+    
+    let imageID = UUID()
+    
+    let imagesRef = Storage.storage().reference().child("images/\(imageID.uuidString).jpg")
+    
+    imagesRef.putData(imageData!, metadata: nil) { (metadata, error) in
+      guard metadata != nil else {
+        print(error!)
+        return
+      }
+      print("image upload sucses")
+    }
+    
+    Firestore.firestore().collection("Users").document(user.uid)
+      .updateData(["imageUrl" : imageID.uuidString]) { (error) in
+        guard error != nil else {
+          print("success update imageUrl")
+          return
+        }
+        print(error!.localizedDescription)
+    }
+  }
+  
+  func downloadImage() {
+    Storage.storage().reference().child("images/\(user.imageUrl).jpg")
+      .getData(maxSize: 1*1024*1024) { (data, error) in
+        guard error == nil else {
+          print(error!.localizedDescription)
+          return
+        }
+        let image = UIImage(data: data!)
+        self.profilemageView.image = image
+    }
   }
   
 }
